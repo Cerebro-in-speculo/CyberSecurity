981445bdb828dfe8f456ffb99bbc2e3f67d15bbe
Cerebro-in-speculo <r.ledovskih@gmail.com>
2026-08-10 20:46:43 +0300

    corr 3

--- server.py
+++ server.py
@@ -3,22 +3,30 @@ import socket
 def run_tcp_server(host = '127.0.0.1', port = 8888):
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # 1. Создаём TCP-сокет (IPv4)
     server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Опция: переиспользовать адрес (позволяет быстро перезапустить сервер)
+    server_sock.settimeout(5.0)                                       # На все операции не более 5 секунд
     server_sock.bind((host, port))                                    # 2. Привязываем сокет к адресу и порту
     server_sock.listen(5)                                             # 3. Переводим сокет в режим ожидания подключений (5 - максимальная длина очереди ожидающих соединений)
     print(f"Сервер запущен на {host}:{port}, ожидает подключений...")
 
-    while True:
-        client_sock, client_addr = server_sock.accept()               # 4. Принимаем новое соединение
-        print(f"Подключился клиент: {client_addr}")
+    try:
+        while True:
+            client_sock, client_addr = server_sock.accept()               # 4. Принимаем новое соединение
+            print(f"Подключился клиент: {client_addr}")
 
-        while client_sock:
-            while True:
-                data = client_sock.recv(1024)                         # читаем до 1024 байт      
-                if not data:                                          # соединение закрыто клиентом
-                    break 
-                print(f"Получено: {data.decode('utf-8')}")            # Отправляем данные обратно (эхо)
-                client_sock.sendall(data)
-        print(f"Клиент {client_addr} отключился")
+            while client_sock:
+                while True:
+                    data = client_sock.recv(1024)                         # читаем до 1024 байт      
+                    if not data:                                          # соединение закрыто клиентом
+                        break 
+                    print(f"Получено: {data.decode('utf-8')}")            # Отправляем данные обратно (эхо)
+                    client_sock.sendall(data)
+            print(f"Клиент {client_addr} отключился")
+    except OSError as e:
+        print(f"Ошибка сокета: {e.errno} – {e.strerror}")
+    finally:
+        server_sock.close()
+        
+
 
 if __name__ == '__main__':
     run_tcp_server()                                
